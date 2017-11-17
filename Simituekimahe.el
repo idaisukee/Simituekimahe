97de55ab7d1203ebb64d27314319e963129fad67
Daisuke Inoue <hibariyashiki@gmail.com>
2017-11-17 17:24:00 +0900

    first

---
+++ Simituekimahe.el
@@ -0,0 +1,22 @@
+(setq ieremii-color
+    (ht
+        ("clean-pondwater" "#a7dbd8")
+        ("beach-storm" "#e0e4cc")
+        ("giant-goldfish" "#f38630")
+        ("unreal-food-pills" "#fa6900")
+        ("lime-green-001" "#89FD00")
+        ("ruby-red" "#912347")
+        ("chestnut" "#5B5139")
+        ("gold" "#A08035")
+        ("dark-gray" "#5A6875")
+        ("grass" "#019477")
+        ("prevert" "#6FAD4C")
+        ("pacman-aqua" "#07EEFF")
+        ("pacman-red" "#F80839")
+        ("pacman-yellow" "#FFFF2D")
+        ("pfizer-blue" "#0069AB")
+        ("pale-lemon" "#FFFFC5")
+        ("red-flashy" "#DF295C")
+        ))
+
+(provide 'Simituekimahe)
